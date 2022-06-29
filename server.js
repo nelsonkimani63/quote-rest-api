02e7446cb73044f98690f9d642d7c7fd1daa617c
nelsonkimani63 <nelsonkimani63@gmail.com>
2022-06-29 13:33:59 +0300

    quote rest api

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+
+
+//GET/quotes -get all  quotes
+//POST/quotes -create a quote
+//PATCH/quotes/:id - update quotes
+//DELETE/quotes/:id -remove quote
